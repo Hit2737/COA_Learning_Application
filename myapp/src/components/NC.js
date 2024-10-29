54693b0f4bbc671d38b49ed70ba5bf736eb742f1
Bhavik Patel <bhavik.patel@iitgn.ac.in>
2024-10-29 11:12:03 +0530

    Fetching number correctly from the input type. Working on showing the steps of conversion

--- myapp/src/components/NC.js
+++ myapp/src/components/NC.js
@@ -1,16 +1,41 @@
 import React from 'react'
 import SelectItem from './NC/select'
-export default function NumberConvertor() {
+import { convertBase } from './NC/conversions'
+import {DisplayResult} from './NC/displayResult'
+import { useState } from 'react'
+
+
+export default function NumberConvertor({mode}) {
+    const [number, setNumber] = useState("Enter valid number to convert");
+    const [fromBase, setFromBase] = useState(10);
+    const [toBase, setToBase] = useState(2);
+    function handleNumber(e){
+        setNumber(e.target.value)
+    }
+    console.log(number,fromBase,toBase)
     return (
         <div>
             <div className="row">
                 <div className='col'>
-                    <SelectItem text="Convert from" id="converFrom"/>
+                    <SelectItem id="converFrom" setBase={setFromBase} base={fromBase}/>
                 </div>
                 <div className='col'>
-                    <SelectItem text="Convert to" id="converTo"/>
+                    <input type="text" id="from" className="form-control" aria-label="Text input with dropdown button" placeholder="Enter valid number to convert to"  onChange={handleNumber}></input>
+                </div>
+                <div className='col'>
+                    <SelectItem id="converTo" setBase={setToBase} base={toBase}/>
+                </div>
+                <div className='col'>
+                    <input type="text" id="to" className="form-control" aria-label="Text input with dropdown button" placeholder='Result'></input>
                 </div>
             </div>
-        </div>
-    )
+
+            <div className="container mt-5">
+                <h2>Conversion Steps</h2>
+                <div className="form-group">
+                    <label htmlFor="conversionSteps">Steps of Conversion:</label>
+                    <DisplayResult number />
+                </div>
+            </div>
+        </div>)
 }
