df79702ac42576fd2b7afb018aad22bc7f3aa3ff
Bhavik Patel <140006006+bp0609@users.noreply.github.com>
2024-11-03 23:16:25 +0530

    Update NC.js

--- myapp/src/components/NC.js
+++ myapp/src/components/NC.js
@@ -1,56 +1,105 @@
-import React from 'react'
-import SelectItem from './NC/select'
-// import { convertBase } from './NC/conversions'
-import { DisplayResult } from './NC/displayResult'
+import DropDown from './NC/DropDown'
+import {DisplaySteps,isValidInput} from './NC/DisplaySteps'
 import { useState } from 'react'
 import IeeeSinglePrecision from './NC/DecimalToIeeeSingle'
 
 
-export default function NumberConvertor({ mode }) {
-    const [number, setNumber] = useState(100101);
+export default function NumberConvertor() {
+    const [number, setNumber] = useState("");
     const [fromBase, setFromBase] = useState(2);
     const [toBase, setToBase] = useState(16);
-    function handleNumber(e) {
+    const [result, setResult] = useState("");
+    const [validInput, setValidInput] = useState(false);
+    const [decimalValue, setDecimalValue] = useState("");
+    const [outputIntVal, setOutputIntVal] = useState("");
+    const [outputFracVal, setOutputFracVal] = useState("");
+
+    function handleNumberChange(e){
         setNumber(e.target.value)
+        if(fromBase===10){
+            setDecimalValue(e.target.value);
+        }
+        checkValidInput(e.target.value,fromBase);
     }
-    // function evaluate() {
+    function checkValidInput(number,fromBase){
+        isValidInput(number,fromBase)?setValidInput(true):setValidInput(false);
+    }
+    function wrongInput(){
+        return <div className="alert alert-danger">Invalid Input</div>
+    }
 
-    //     convertBase(number, fromBase, toBase)
-    // }
-
-    console.log(number, fromBase, toBase)
+    function noConversion(){
+        return (
+            <div className="alert alert-info my-2">No conversion needed</div>
+        );
+    }
+    function handleConversion(){
+        if(!validInput){
+            setResult("");
+        }
+        else if(fromBase===toBase){
+            setResult(number);
+        }
+        else if(toBase===10){
+            setResult(decimalValue);
+        }
+        else{
+            if(outputFracVal===""){
+                setResult(`${outputIntVal}`);
+            }else{
+                setResult(`${outputIntVal}.${outputFracVal}`);
+            }
+        }
+    }
+    
     return (
         <>
             <h1 className="text-center my-3">Number System Converter</h1>
             <div className="container-fluid">
                 <div className="row">
                     <div className='col'>
-                        <SelectItem id="converFrom" setBase={setFromBase} base={fromBase} />
+                        <DropDown id="convertFrom" setBase={setFromBase} base={fromBase} setValidInput={setValidInput} number={number} setDecimalValue={setDecimalValue}/>
                     </div>
                     <div className='col'>
-                        <input type="text" id="from" className="form-control" aria-label="Text input with dropdown button" placeholder="Enter valid number to convert to" onChange={handleNumber}></input>
+                        <input type="text" id="from" className="form-control" aria-label="Text input with dropdown button" placeholder="Enter valid number to convert to"  onChange={handleNumberChange}></input>
                     </div>
                     <div className='col'>
-                        <SelectItem id="converTo" setBase={setToBase} base={toBase} />
+                        <DropDown id="convertTo" setBase={setToBase} base={toBase} setValidInput={setValidInput} number={number}/>
                     </div>
                     <div className='col'>
-                        <input type="text" id="to" className="form-control" aria-label="Text input with dropdown button" placeholder='Result'></input>
+                    <input type="text" id="to" className="form-control" aria-label="Text input with dropdown button" placeholder="Result" value={result} readOnly/>
+
                     </div>
                 </div>
 
                 <div className="row">
                     <div className="my-3">
-                        <button className="btn btn-secondary">Convert </button>
-                    </div>
+                        <button className="btn btn-secondary" onClick={handleConversion}>Convert </button>
+                    </div>   
                 </div>
             </div>
 
             <div className="container mt-5">
                 <h2>Conversion Steps</h2>
-                <div className="form-group">
-                    <label htmlFor="conversionSteps">Steps of Conversion:</label>
-                    <DisplayResult number={number} fromBase={fromBase} toBase={toBase} />
-                </div>
+                {(!validInput)? wrongInput():
+                    (<>{(fromBase===toBase)? noConversion():
+                        (<div className="form-group">
+                            <label htmlFor="conversionSteps">Steps of Conversion:</label>
+                            <DisplaySteps 
+                                number={number.toUpperCase()} 
+                                fromBase={fromBase} 
+                                toBase={toBase} 
+                                setResult={setResult} 
+                                decimalValue={decimalValue} 
+                                setDecimalValue={setDecimalValue}
+                                setOutputFracVal={setOutputFracVal}
+                                setOutputIntVal={setOutputIntVal}
+                                outputFracVal={outputFracVal}
+                                outputIntVal={outputIntVal}/>
+                        </div>)
+                    }</>)
+                }
+
             </div>
             {/* <IeeeSinglePrecision /> */}
         </>)
