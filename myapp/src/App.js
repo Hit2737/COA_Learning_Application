da224ada4f0b151610e4d27ed5bbe9c6358b917a
Bp0306 <bhavik.patel@iitgn.ac.in>
2024-11-12 11:39:26 +0530

    Removed about page and home button from navbar.(No offense).

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -47,7 +47,7 @@ function App() {
         <div className="container my-3">
           <Routes>
             <Route exact path={'/'} element={<ToolBox mode={mode} />} />
-            <Route exact path={'/about'} element={<About />} />
+            {/* <Route exact path={'/about'} element={<About />} /> */}
             <Route exact path={'/performacemetricanalyser'} element={<PerformanceMetricAnalyser mode={mode} showAlert={showAlert} />} />
             <Route exact path={'/cachesimulator'} element={<CacheSimulator mode={mode} showAlert={showAlert} />} />
             <Route exact path={'/numberconvertor'} element={<NumberConvertor mode={mode} showAlert={showAlert} />} />
--- myapp/src/components/Navbar.js
+++ myapp/src/components/Navbar.js
@@ -10,8 +10,8 @@ export default function Navbar({ title = 'NavBar', mode = 'light', toggleMode })
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item"><Link className="nav-link" to="/">Home</Link></li>
-                        <li className="nav-item"><Link className="nav-link" to="/about">About</Link></li>
+                        {/* <li className="nav-item"><Link className="nav-link" to="/">Home</Link></li> */}
+                        {/* <li className="nav-item"><Link className="nav-link" to="/about">About</Link></li> */}
                     </ul>
                     <label className="form-check-label" htmlFor="flexSwitchCheckDefault">{(mode === 'light') ? 'Light' : 'Dark'} mode</label>
                     <div className={`form-check form-switch mx-2 ${(mode === 'dark') ? "text-light" : ""}`}>
